daccbcb89324e370e1783a494170e38ee979d2e7
N4CH77 <n4ch77@gmail.com>
2025-11-12 14:46:51 -0300

    Feat: Implementa a função Atualizar (Update)

--- documento.h
+++ documento.h
@@ -4,15 +4,16 @@
 // -- Estrutura para leitura ou criação do documento (CPF, nome, idade e sexo) da empresa --
 struct Documento
 {
-    char cpf[15]; // "char" guarda um texto. No exemplo, 15 caracteres
+    char cpf[20]; // "char" guarda um texto. No exemplo, 15 caracteres
     char nome[100];
     int idade; // "int" guarda números
     char sexo[20];
 };
 
 // -- Declaração das funções p/ main() [3 funções] --
-void criarDocumento(); // Linha 61
-void lerDocumentos(); // Linha 106
-void limparTela(); // Linha 131
+void criarDocumento();
+void lerDocumentos(); 
+void atualizarDocumento();
+void limparTela();
 
 #endif
